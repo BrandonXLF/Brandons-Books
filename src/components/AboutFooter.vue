ab74495669fae18fe57cdd15fe9059475987a352
Brandon Fowler <42590338+BrandonXLF@users.noreply.github.com>
2023-12-10 16:05:12 -0500

    Add source code link

--- src/components/AboutFooter.vue
+++ src/components/AboutFooter.vue
@@ -1,4 +1,4 @@
 <template>
 	Online bookkeeping program by
-	<a href="https://www.brandonfowler.me/">Brandon Fowler</a>.
+	<a href="https://www.brandonfowler.me/">Brandon Fowler</a>. <a href="https://github.com/BrandonXLF/Brandons-Books/">View source code</a>.
 </template>
