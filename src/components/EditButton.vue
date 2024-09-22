e14a8ab091072d2956333d8fff45786d5c54f7de
Brandon Fowler <brandfowl@gmail.com>
2024-09-22 17:31:02 -0400

    Add SVGButton component

--- src/components/EditButton.vue
+++ src/components/EditButton.vue
@@ -1,18 +1,20 @@
-<template>
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 24 24"
-		stroke-width="2"
-		stroke="currentColor"
-		fill="none"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-	>
-		<path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4"></path>
-		<line x1="13.5" y1="6.5" x2="17.5" y2="10.5"></line>
-	</svg>
-</template>
+<script setup lang="ts">
+	import SVGButton from './SVGButton.vue';
+</script>
 
-<style scoped>
-	@import url('@/assets/svg-button.css');
-</style>
+<template>
+	<SVGButton>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			stroke-width="2"
+			stroke="currentColor"
+			fill="none"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		>
+			<path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4"></path>
+			<line x1="13.5" y1="6.5" x2="17.5" y2="10.5"></line>
+		</svg>
+	</SVGButton>
+</template>
---
+++ src/components/SVGButton.vue
@@ -0,0 +1,20 @@
+<template>
+	<button>
+		<slot />
+	</button>
+</template>
+
+<style scoped>
+	button {
+		padding: 0;
+		margin: 0;
+		background: none;
+		margin-left: 0.5em;
+		font: inherit;
+	}
+
+	button > :deep(svg) {
+		height: 1em;
+		cursor: pointer;
+	}
+</style>
